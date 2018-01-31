095a7a1194b2b02643240aaa05a931c6c7b8716d
lagranmoon <lagranmoon@outlook.com>
2018-01-31 18:05:30 +0800

    init:init the project and update readme

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # MEditor
 Android端Markdown编辑器
-* 适配手机和平板
-* Markdwon标准语法和[GFM](https://guides.github.com/features/mastering-markdown/#GitHub-flavored-markdown)语法支持
-* 数学公式的支持
-* 第三方图床适配（七牛，腾讯OSS，阿里OSS等)
+1. 基础需求
+	- [ ] markdown基本语法支持
+	- [ ] Github Favored Mardown语法支持
+	- [ ] 快捷输入栏&&预览功能
+	- [ ] 文档库支持
+	- [ ] 七牛云，阿里OSS，腾讯OSS等图床支持
+	- [ ] Dropbox，坚果云，Github同步支持
+2. 次级需求
+	- [ ] 主题功能&&Material Design设计
+	- [ ] 导出功能（PDF)
+	- [ ] 时记模式，按时间整理
+3. 可能实现的需求
+	- [ ] 同步印象笔记(需要用户提供APIToken)，类似马克飞象
+	- [ ] 使用PanDoc提供多种导出功能
---
+++ app/src/main/java/com/lagranmoon/meditor/util/ActivityUtil.java
@@ -0,0 +1,42 @@
+package com.lagranmoon.meditor.util;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+/**
+     * Created by lagranmoon on 2017/8/21.
+     * This provides methods to help Activities load their UI.
+ * */
+
+public class ActivityUtil {
+
+    /**
+     * 添加Fragment到布局容器中
+     * @param fragmentManager 传入的FragmentManager
+     * @param fragment 要添加的Fragment
+     * @param containerId 要添加到的布局容器ID
+     */
+    public static void addFragment(@NonNull FragmentManager fragmentManager,
+                                   @NonNull Fragment fragment, int containerId) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(containerId, fragment);
+        transaction.commit();
+    }
+
+    /**
+     * 替换布局容器里的Fragment
+     * @param fragmentManager 传入的FragmentManager
+     * @param fragment 要替换成的Fragment
+     * @param containerId 被替换的布局容器
+     */
+
+    public static void replaceFragment(@NonNull FragmentManager fragmentManager,
+                                       @NonNull Fragment fragment, int containerId) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(containerId, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
