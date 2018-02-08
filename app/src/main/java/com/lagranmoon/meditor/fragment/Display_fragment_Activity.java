dca59ff4a0abbeb4567aa3ea79df9b03f3f25024
xmmmmmovo <13256172360@163.com>
2018-02-08 16:25:21 +0800

    2018.02.08修改

---
+++ app/src/main/java/com/lagranmoon/meditor/fragment/Display_fragment_Activity.java
@@ -0,0 +1,15 @@
+package com.lagranmoon.meditor.fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lagranmoon.meditor.R;
+
+public class Display_fragment_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display_fragment_activity);
+    }
+}
